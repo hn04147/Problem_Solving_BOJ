badd9ee9ed379c3560fb3babf65569ef2236c526
hn04147 <hn04147@naver.com>
2019-12-31 20:43:33 +0900

    9095.py

---
+++ 9095.py
@@ -0,0 +1,15 @@
+#dp(num) = dp(num-1) + dp(num-2) + dp(num-3)
+
+list_ = [0] * 12
+
+def solution(num):
+    global list_
+    if num==1: return 1
+    if num==2: return 2
+    if num==3: return 4
+    if list_[num]!=0: return list_[num]
+    list_[num] = solution(num-1) + solution(num-2) + solution(num-3)
+    return list_[num]
+
+
+
