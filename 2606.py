28ce4075cd3bae2e084d9b0cc3a4347f9745331e
Lee Sangjin <hn04147@fgb.ai>
2021-09-08 21:20:52 +0900

    2021.9.8

---
+++ 2606.py
@@ -0,0 +1,33 @@
+'''
+Q:
+7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7
+
+A: 4
+'''
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
+  x, y = map(int, input().split())
+  graph[x].append(y)
+  graph[y].append(x)
+
+visited = []
+queue = [1]
+
+while queue:
+  num = queue.pop(0)
+  if num not in visited:
+    visited.append(num)
+    for i in graph[num]:
+      queue.append(i)
+
+print(len(visited) - 1)
