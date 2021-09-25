71106a3f1bc993afae368d07251ec2eb75d4976c
Sangjin <hn04147@naver.com>
2021-09-25 19:01:23 +0900

    2021.9.25

---
+++ CLASS_3/1620.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().strip().split())
+book_1 = {}
+book_2 = {}
+for i in range(1, n + 1):
+  pk = input().strip()
+  book_1[i] = pk
+  book_2[pk] = i
+
+print(book_1)
+print(book_2)
+
+for _ in range(m):
+  idx = input().strip()
+  if idx.isdigit():
+    print(book_1[int(idx)])
+  else:
+    print(book_2[idx])
