a5a2821e59d7cee101a09876ccbb99a6eef172ea
Sangjin <hn04147@naver.com>
2021-10-12 01:49:27 +0900

    2021.10.11

---
+++ CLASS_5/2166.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
+arr.append(arr[0])
+
+left, right = 0, 0
+for i in range(0, n):
+  left += arr[i][0] * arr[i + 1][1]
+  right += arr[i][1] * arr[i + 1][0]
+print((left - right) / 2)
