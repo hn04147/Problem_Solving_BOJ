b1b583daa628e0c77efc582d712a95ec9fc1c993
Sangjin <hn04147@naver.com>
2021-10-30 15:19:02 +0900

    2021.10.30

---
+++ 2470.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+arr = sorted(list(map(int, input().rstrip().split())))
+l, r = 0, n - 1
+max_ = sys.maxsize
+ans = []
+
+while l != r:
+  if abs(arr[l] + arr[r]) < max_:
+    max_ = abs(arr[l] + arr[r])
+    ans = [l, r]
+  if arr[l] + arr[r] < 0:
+    l += 1
+  else:
+    r -= 1
+
+print(arr[ans[0]], arr[ans[1]])
