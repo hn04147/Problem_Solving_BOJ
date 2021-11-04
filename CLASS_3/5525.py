a33fddc1239a8068eb4c96d2df3ac4064248ef81
Sangjin <hn04147@naver.com>
2021-11-04 20:33:02 +0900

    2021.11.4

--- CLASS_3/5525.py
+++ CLASS_3/5525.py
@@ -5,17 +5,32 @@ n = int(input().strip())
 t = int(input().strip())
 arr = input().strip()
 
-target = 'I'
-for _ in range(n):
-  target += 'OI'
+# target = 'I'
+# for _ in range(n):
+#   target += 'OI'
+
+# cnt = 0
+# idx = 0
+# while idx < t - 2 * n - 1:
+#   if arr[idx:idx + 2*n + 1] == target:
+#     cnt += 1
+#     idx += 2
+#   else:
+#     idx += 1
 
 cnt = 0
 idx = 0
-while idx < t - 2 * n - 1:
-  if arr[idx:idx + 2*n + 1] == target:
-    cnt += 1
-    idx += 2
-  else:
+part = 0
+
+while idx < (t - 2):
+  if arr[idx] == 'I' and arr[idx + 1] == 'O' and arr[idx + 2] == 'I':
+    part += 1
+    if part == n:
+      cnt += 1
+      part -= 1
     idx += 1
+  else:
+    part = 0
+  idx += 1
 
 print(cnt)
