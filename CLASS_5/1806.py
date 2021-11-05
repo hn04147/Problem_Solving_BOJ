090c545d328cdbd2e96aae014b556d00e78932ac
Sangjin <hn04147@naver.com>
2021-11-05 19:07:59 +0900

    2021.11.5

---
+++ CLASS_5/1806.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().rstrip().split())
+arr = list(map(int, input().rstrip().split()))
+
+min_len = 10001
+l, r = 0, 0
+tmp_sum = arr[0]
+
+while True:
+  if tmp_sum >= s:
+    min_len = min(min_len, r - l + 1)
+    tmp_sum -= arr[l]
+    l += 1
+  else:
+    r += 1
+    if r == n:
+      break
+    tmp_sum += arr[r]
+
+print(0 if min_len == 10001 else min_len)
