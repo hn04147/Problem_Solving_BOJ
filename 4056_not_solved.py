20285e9bdaf5045448442f41c9540b9022f7ecd4
Sangjin <hn04147@naver.com>
2021-11-06 00:27:35 +0900

    2021.11.5

---
+++ 4056_not_solved.py
@@ -0,0 +1,60 @@
+import sys
+input = sys.stdin.readline
+
+def check_row(i, x, y):
+  for j in range(9):
+    if arr[x][j] == i:
+      return False
+  return True
+
+def check_col(i, x, y):
+  for j in range(9):
+    if arr[j][y] == i:
+      return False
+  return True
+
+def check_3x3(i, x, y):
+  for m in range(x // 3 * 3, x // 3 * 3 + 3):
+    for n in range(y // 3 * 3, y // 3 * 3 + 3):
+      if arr[m][n] == i:
+        return False
+  return True
+
+def track(n):
+  global flag
+  x = n // 9
+  y = n % 9
+
+  if flag:
+    return
+
+  if n == 81:
+    flag = True
+    for i in range(9):
+      for j in range(9):
+        print(arr[i][j], end = '')
+      print()
+    print()
+    return
+
+  if arr[x][y] != 0:
+    track(n + 1)
+  else:
+    for i in range(1, 10):
+      if check_row(i, x, y) and check_col(i, x, y) and check_3x3(i, x, y):
+        arr[x][y] = i
+        track(n + 1)
+        arr[x][y] = 0
+    
+    if not flag:
+      print('Could not complete this grid.')
+      print()
+      flag = True
+  
+  return
+
+n = int(input())
+for _ in range(n):
+  arr = [list(map(int, input().rstrip())) for _ in range(9)]
+  flag = False
+  track(0)
